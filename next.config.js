db5f790f3e7dd5be1daad4f1a1183778a60d2607
Nelwin Catalogo <catalogonelwin@gmail.com>
2022-12-26 13:46:57 +0800

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // trailingSlash: true,
+  // compiler: { // uncomment this to enable console logs remover
+  //   removeConsole: {
+  //     exclude: ['error'],
+  //   },
+  // },
+};
+
+module.exports = nextConfig;
